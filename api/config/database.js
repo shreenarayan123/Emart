834faa59c209ac1de5a41d295d0de3f2c9403191
Your Name <127131074+shreenarayan123@users.noreply.github.com>
2024-08-16 02:05:21 +0530

    adding CA certificate

--- api/config/database.js
+++ api/config/database.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path');
 
 module.exports = ({ env }) => {
@@ -13,14 +14,10 @@ module.exports = ({ env }) => {
         database: env('DATABASE_NAME'),
         user: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL') && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-         
-        },
+        ssl: env.bool('DATABASE_SSL', false) ? {
+          rejectUnauthorized: false,
+          ca: env('DATABASE_CA_CERT') ? env('DATABASE_CA_CERT').replace(/\\n/g, '\n') : undefined,
+        } : false,
         schema: env('DATABASE_SCHEMA', 'public'),
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
