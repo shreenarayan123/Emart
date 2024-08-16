b96b52b9bd5f448df0511ea8ef55f311f12d97f7
Your Name <you@example.com>
2024-08-16 22:42:15 +0530

    added cors config

--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -1,7 +1,27 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://emart-k5vl.vercel.app'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://emart-k5vl.vercel.app'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['https://emart-k5vl.vercel.app']
+    }
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
