c1bca67505e9d1c163722fc028a1a03948d1f6b7
Your Name <127131074+shreenarayan123@users.noreply.github.com>
2024-08-17 17:02:43 +0530

    updated for origin

--- api/config/server.js
+++ api/config/server.js
@@ -1,10 +1,13 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://emart-pzxc.onrender.com',
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+  },
   app: {
     keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
